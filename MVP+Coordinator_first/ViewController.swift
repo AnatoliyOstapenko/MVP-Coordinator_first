6bc438019b1efa33ef981ea5bd5f2635c6a2f287
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-03-25 15:10:44 +0200

    Initial Commit

---
+++ MVP+Coordinator_first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP+Coordinator_first
+//
+//  Created by AnatoliiOstapenko on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
